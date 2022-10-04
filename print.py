fffffac79d818837eb6194e52fdf901b951c79e2
Asif_Ahamed <mdasifahamedfahim@gmail.com>
2022-10-04 13:01:18 +0600

    Types of Printing in Pyhton

---
+++ print.py
@@ -0,0 +1,18 @@
+#simple print
+print('Hello Visitor\n')
+
+#display "" int he console exm: Hello "Visitor"
+print('Hello "Visitor"\n')
+
+#display ' in the middel of the words int he console exm: Hello Visitor's
+print("Hello Visitor's\n")
+
+#display Line Break using \n  of the words int he console exm: Hello Visitor's
+#                                                              How are You?
+print("Hello Visitor's \nHow are you ?\n")
+
+#parargraph printing using """ """
+
+print("""Hello Visitor's 
+How are you ? 
+Are You Ready For Pyhtonic Fun?""")
