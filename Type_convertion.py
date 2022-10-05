e818749ccd0b3f158aa9618d4eb456b6b7a3845e
Asif_Ahamed <mdasifahamedfahim@gmail.com>
2022-10-05 12:09:45 +0600

    How To  Convert Data Type

---
+++ Type_convertion.py
@@ -0,0 +1,25 @@
+#Type Converion Types in Python.
+a = "jack is "
+b = 10
+c = " years old"
+print(a+str(b)+c)
+
+#type converting while assigning value to variable
+
+x =int("10")
+y = 30
+print(x + y)
+
+#finding data types
+
+s = "Abac"
+d = 10
+f = 10.5
+g = True
+print(type(s))
+print(type(d))
+print(type(f))
+print(type(g))
+
+
+
